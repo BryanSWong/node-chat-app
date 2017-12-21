525e8468ce246244a33ce3ed84230afdf4de5156
Bryan <Bwong727@gmail.com>
2017-12-20 22:08:01 -0500

    Completed video 8 and 9, added form for messages and show incoming messages to browser.

--- public/js/index.js
+++ public/js/index.js
@@ -22,8 +22,31 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
     console.log('New message received', message);
+    let li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+
+    jQuery('#messages').append(li);
+});
+
+
+jQuery('#message-form').on('submit', function(e){
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    }, function() {});
 });
 
 // socket.on('newEmail', function (email) {
 //     console.log('new email', email);
+// });
+
+
+// socket.emit('createMessage', {
+//     from: 'Frank',
+//     text: 'Hi'
+// }, function (data) {
+//     console.log('Got it', data);
+//
 // });
